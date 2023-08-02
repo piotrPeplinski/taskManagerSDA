530c33be4feff87a37a01b5feed71d7d360c35cd
Piotr Pepliński <p.peplaj@gmail.com>
2023-08-02 19:52:07 +0200

    created Task model

--- taskManager/tasks/admin.py
+++ taskManager/tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+
+class CreateDate(admin.ModelAdmin):
+    readonly_fields = ('createDate',)
+
+admin.site.register(Task, CreateDate)
