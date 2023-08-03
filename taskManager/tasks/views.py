2a7ad9d6eaffc05b7d995abe25545d25bd21a3f3
Piotr Pepliński <p.peplaj@gmail.com>
2023-08-03 17:48:03 +0200

    added create task functionality

--- taskManager/tasks/views.py
+++ taskManager/tasks/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
 
+
+def tasks(request):
+    usersTasks = Task.objects.filter(user=request.user)
+    return render(request, 'tasks.html', {'tasks': usersTasks})
+
+
+def create(request):
+    if request.method == 'GET':
+        return render(request, 'create.html', {'form': TaskForm()})
+    else:  # post
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('tasks')
+        else:
+            error = 'Something went wrong.'
+            return render(request, 'create.html', {'form': TaskForm(),'error':error})
