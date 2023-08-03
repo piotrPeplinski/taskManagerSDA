c191423630f4abda5c9b5d9b7c75a702b01ebcbf
Piotr Pepliński <p.peplaj@gmail.com>
2023-08-03 20:46:25 +0200

    finished project

--- taskManager/authSystem/views.py
+++ taskManager/authSystem/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -74,6 +75,7 @@ def login_user(request):
             return render(request, 'login_user.html', {'form': AuthenticationForm(), 'error': error})
 
 
+@login_required
 def logout_user(request):
     logout(request)
     return redirect('home')
--- taskManager/tasks/views.py
+++ taskManager/tasks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -9,6 +10,7 @@ def home(request):
     return render(request, 'home.html')
 
 
+@login_required
 def tasks(request):
     current = Task.objects.filter(user=request.user, completeDate__isnull=True)
     completed = Task.objects.filter(
@@ -16,6 +18,7 @@ def tasks(request):
     return render(request, 'tasks.html', {'current': current, 'completed': completed})
 
 
+@login_required
 def create(request):
     if request.method == 'GET':
         return render(request, 'create.html', {'form': TaskForm()})
@@ -31,14 +34,16 @@ def create(request):
             return render(request, 'create.html', {'form': TaskForm(), 'error': error})
 
 
+@login_required
 def delete_task(request, taskId):
-    task = get_object_or_404(Task, id=taskId)
+    task = get_object_or_404(Task, id=taskId, user=request.user)
     task.delete()
     return redirect('tasks')
 
 
+@login_required
 def detail(request, taskId):
-    task = get_object_or_404(Task, id=taskId)
+    task = get_object_or_404(Task, id=taskId, user=request.user)
     if request.method == 'GET':
         form = TaskForm(instance=task)
         return render(request, 'detail.html', {'form': form, 'task': task})
@@ -52,8 +57,9 @@ def detail(request, taskId):
             return render(request, 'detail.html', {'form': form, 'task': task, 'error': error})
 
 
+@login_required
 def complete_task(request, taskId):
-    task = get_object_or_404(Task, id=taskId)
+    task = get_object_or_404(Task, id=taskId, user=request.user)
     task.completeDate = timezone.now()
     task.save()
     return redirect('tasks')
